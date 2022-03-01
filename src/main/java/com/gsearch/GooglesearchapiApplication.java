ba344d1851e221b49576a69a7b4e6bd4c3a90053
Udhayabalachandar <udhaya.chandrasekaran@5datainc.com>
2022-03-01 22:43:49 +0100

    tech(base) add inital setup, PR template

---
+++ src/main/java/com/gsearch/GooglesearchapiApplication.java
@@ -0,0 +1,13 @@
+package com.gsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglesearchapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GooglesearchapiApplication.class, args);
+    }
+
+}
