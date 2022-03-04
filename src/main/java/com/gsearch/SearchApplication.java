4640885191c41e76458640ff108ff9b3b7fabc9c
jpukg <j.prakashuk@gmail.com>
2022-03-04 12:23:49 +0100

    Add Swagger documentation (#12)
    * tech(deployment): Add a property file to enable java11 on Heroku
    
    * tech(documentation): Add swagger page using springdoc-openapi
    
    * tech(cleanup): Add non git files to .gitignote
    
    * tech(format): Fix code indent

---
+++ src/main/java/com/gsearch/SearchApplication.java
@@ -0,0 +1,13 @@
+package com.gsearch;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class SearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchApplication.class, args);
+	}
+
+}
