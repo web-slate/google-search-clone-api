e2ad64a8452a0c4439eebf92bc01c7f88bdfb1b6
jayaprakash <j.prakashuk@gmail.com>
2022-03-08 10:38:31 +0100

    tech(swagger): Fix swagger URL with HTTPS access

--- src/main/java/com/gsearch/config/SwaggerConfig.java
+++ src/main/java/com/gsearch/config/SwaggerConfig.java
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.*;
 
 import io.swagger.v3.oas.models.*;
 import io.swagger.v3.oas.models.security.*;
+import io.swagger.v3.oas.models.servers.*;
 
 @Configuration
 public class SwaggerConfig {
 
 	@Bean
 	public OpenAPI openAPI() {
-		return new OpenAPI().components(
+		return new OpenAPI().addServersItem(new Server().url("/")).components(
 				new Components().addSecuritySchemes("OAuth", new SecurityScheme().type(SecurityScheme.Type.APIKEY)
 						.scheme("bearer").bearerFormat("jwt").in(SecurityScheme.In.HEADER).name("Authorization")));
 	}
